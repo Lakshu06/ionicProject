6e3f8ad378f31bdd297d77c9f38be781649cada5
Lakshu06 <lakshitasaini06@gmail.com>
2019-11-06 18:54:30 +0530

    IonicApp V1

---
+++ mango/src/app/home/home.router.ts
@@ -0,0 +1,33 @@
+// tslint:disable-next-line: eofline
+import { NgModule } from '@angular/core';
+import {RouterModule , Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component : HomePage,
+        children: [
+            {
+              path: 'feed',
+              loadChildren: () => import ('../pages/feed/feed.module').then(n => n.FeedPageModule)
+            },
+            {
+                path: 'notifications',
+                loadChildren: () => import ('../pages/notifications/notifications.module').then(n => n.NotificationsPageModule)
+              },
+              {
+                path: 'messages',
+                loadChildren: () => import ('../pages/messages/messages.module').then(n => n.MessagesPageModule)
+              },
+              {
+                path: 'settings',
+                loadChildren: () => import ('../pages/settings/settings.module').then(n => n.SettingsPageModule)
+              }
+        ]
+    }];
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class HomeRouter {}
